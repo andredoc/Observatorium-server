eff60c2ce0c811beb94df64ca44f2471d1476734
andredoc <andre.documet@gmail.com>
2022-03-08 17:25:41 +0100

    first commit

---
+++ routes/admin.routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router()
+
+const Item = require("./../models/Item.model")
+const User = require("./../models/User.model")
+
+
+
+router.get("/getAllUsers", (req, res) => {
+    User
+        .find()
+        .select("username imgProfile")
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+
+
+
+module.exports = router
